b1a3322e13e5325956515ea5630f77e936d9e692
nightmarexivplugins@gmail.com <5073202+Limiana@users.noreply.github.com>
2026-06-06 06:59:02 +0300

    Update

---
+++ AutoRetainer/UI/NeoUI/AdvancedEntries/DebugSection/DebugCabinet.cs
@@ -0,0 +1,26 @@
+﻿using ECommons.ExcelServices;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using Lumina.Excel.Sheets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Cabinet = Lumina.Excel.Sheets.Cabinet;
+
+namespace AutoRetainer.UI.NeoUI.AdvancedEntries.DebugSection;
+
+public unsafe class DebugCabinet : DebugSectionBase
+{
+    public override void Draw()
+    {
+        ImGuiEx.Text($"State: {UIState.Instance()->Cabinet.State}");
+        foreach(var x in Cabinet.Values)
+        {
+            if(x.Item.RowId != 0)
+            {
+                ImGuiEx.Text(UIState.Instance()->Cabinet.IsItemInCabinet(x.RowId)?EColor.GreenBright:null, $"{ExcelItemHelper.GetName(x.Item.RowId, true)}");
+            }
+        }
+    }
+}
